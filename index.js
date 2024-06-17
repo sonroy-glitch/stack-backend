2ccd57837e0fbaba7c2b3d2f89e6d1bd5aef67c8
Sounak Roy <135100128+sonroy-glitch@users.noreply.github.com>
2024-06-17 14:01:29 -0400

    don env crash fixed

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const zod_1 = require("zod");
-require('dotenv').config()
 const cors_1 = __importDefault(require("cors"));
 const client_1 = require("@prisma/client");
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
